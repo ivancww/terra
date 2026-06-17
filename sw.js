19c2084359d31b56e5016c82f6902b44cedfc43b
ivancww <wahchiu0903@gmail.com>
2026-06-17 15:08:24 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1.7.1'; 
+const CACHE_VERSION = 'v1.8.0'; 
 const CACHE_NAME = `terra-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [ './', './index.html', './manifest.json', './terra-192.png' ];
